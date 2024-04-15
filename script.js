36366552af38adb04630a3b28cc1320c5cc73df2
yajasarora <112699544+yajasarora@users.noreply.github.com>
2024-04-15 15:42:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 // Function to fetch weather data from the API
 async function getWeather() {
-    const apiKey = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
-    const cityInput = document.getElementById('cityInput').value.trim();
+    const apiKey = 'df3468ae6dcabdad0eafdaa8fbee912b'; // My API Key
+    const lat = document.getElementById('Latitude').value.trim();
+    const lon = document.getElementById('Longitude').value.trim();
+    
 
-    // Check if the city input is empty
-    if (cityInput === '') {
-        alert('Please enter a city name.'); // Alert the user to enter a city name
-        return; // Exit the function if city input is empty
-    }
+    
 
     // Construct the API URL for fetching weather data
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     
     try {
         // Make an asynchronous API request using fetch
         const response = await fetch(apiUrl);
         const data = await response.json(); // Parse the JSON response
-
-        // Check if the API response contains valid data
-        if (data.cod !== 200) {
-            throw new Error(data.message); // Throw an error if the API response is not successful
-        }
-
+        console.log(data);
+        7.46
         // Call the displayWeatherData function to update the UI with weather information
         displayWeatherData(data);
     } catch (error) {
@@ -35,10 +29,10 @@ function displayWeatherData(data) {
     const weatherInfo = document.getElementById('weatherInfo'); // Get the weather info container
 
     // Extract relevant weather data from the API response
-    const temperature = data.main.temp;
-    const humidity = data.main.humidity;
-    const windSpeed = data.wind.speed;
-    const weatherDescription = data.weather[0].description;
+    const temperature = data.current.temp;
+    const humidity = data.current.humidity;
+    const windSpeed = data.current.wind_speed;
+    const weatherDescription = data.current.weather[0].description;
 
     // Update the weather info container with the extracted data
     weatherInfo.innerHTML = `
